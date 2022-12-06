df9a4abb8261778a2abfae9da159ee18b2b53d1f
Giuseppe Cammarota <g.cammarota@bopen.eu>
2022-12-06 12:34:23 +0100

    fix formatting

--- c3s_eqc_automatic_quality_control/plot.py
+++ c3s_eqc_automatic_quality_control/plot.py
@@ -16,9 +16,9 @@ This module offers plot functions to visualise diagnostic results.
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any
 
 import plotly.express as px
+import plotly.graph_objs as go
 import xarray as xr
 
 VAR_NAMES_MAP = {
@@ -31,7 +31,7 @@ def line_plot(
     ds: xr.Dataset,
     var: str,
     title: str = "",
-) -> Any:
+) -> go.Figure:
     try:
         fig = px.line(
             x=ds["time"],
