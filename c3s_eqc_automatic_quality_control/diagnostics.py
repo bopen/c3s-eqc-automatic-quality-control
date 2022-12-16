3f3124f360290ec4ae39c0ee96752c5560f8583f
Mattia Almansi <m.almansi@bopen.eu>
2022-12-16 14:27:42 +0100

    add notebook insitu

--- c3s_eqc_automatic_quality_control/diagnostics.py
+++ c3s_eqc_automatic_quality_control/diagnostics.py
@@ -17,33 +17,81 @@ This module gathers available diagnostics.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Union
+
 import numpy as np
 import xarray as xr
 
+LONS = ("longitude", "lon")
+LATS = ("latitude", "lat")
 
-def spatial_mean(
-    ds: xr.Dataset | xr.DataArray, lon: str = "longitude", lat: str = "latitude"
-) -> xr.Dataset | xr.DataArray:
+
+def get_coord_name(obj, valid_values):
+    coords = [var for var in obj.coords if var.lower() in valid_values]
+    if len(coords) == 1:
+        return coords[0]
+    raise ValueError("can NOT infer coordinate names")
+
+
+def get_lon_lat_names(
+    obj: Union[xr.Dataset, xr.DataArray], lon: str | None = None, lat: str | None = None
+) -> tuple[str, str]:
+    return (lon or get_coord_name(obj, LONS), lat or get_coord_name(obj, LATS))
+
+
+def spatial_weights(
+    obj: Union[xr.Dataset, xr.DataArray], lon: str | None = None, lat: str | None = None
+):
+    lon, lat = get_lon_lat_names(obj, lon, lat)
+    cos = np.cos(np.deg2rad(obj[lat]))
+    return cos / (cos.sum(lat) * len(obj[lon]))
+
+
+def spatial_weighted_mean(
+    obj: Union[xr.Dataset, xr.DataArray], lon: str | None = None, lat: str | None = None
+) -> Union[xr.Dataset, xr.DataArray]:
     """
     Calculate spatial mean of ds with latitude weighting.
 
     Parameters
     ----------
-    ds: xr.Dataset or xr.DataArray
+    obj: xr.Dataset or xr.DataArray
         Input data on which to apply the spatial mean
     lon: str, optional
         Name of longitude coordinate
     lat: str, optional
         Name of latitude coordinate
+
     Returns
     -------
-    ds spatially mean
+    reduced object
     """
-    cos = np.cos(np.deg2rad(ds[lat]))
-    weights = cos / (cos.sum(lat) * len(ds[lon]))
-    return (ds * weights).sum(dim=[lon, lat])  # type: ignore
+    lon, lat = get_lon_lat_names(obj, lon, lat)
+    with xr.set_options(keep_attrs=True):
+        weights = spatial_weights(obj, lon, lat)
+        return obj.weighted(weights).mean((lon, lat))
 
 
-def spatial_daily_mean(ds: xr.Dataset | xr.DataArray) -> xr.Dataset | xr.DataArray:
-    ds = spatial_mean(ds)
-    return ds.resample(time="1D").mean("time")
+def spatial_weighted_std(
+    obj: Union[xr.Dataset, xr.DataArray], lon: str | None = None, lat: str | None = None
+) -> Union[xr.Dataset, xr.DataArray]:
+    """
+    Calculate spatial std of ds with latitude weighting.
+
+    Parameters
+    ----------
+    obj: xr.Dataset or xr.DataArray
+        Input data on which to apply the spatial mean
+    lon: str, optional
+        Name of longitude coordinate
+    lat: str, optional
+        Name of latitude coordinate
+
+    Returns
+    -------
+    reduced object
+    """
+    lon, lat = get_lon_lat_names(obj, lon, lat)
+    with xr.set_options(keep_attrs=True):
+        weights = spatial_weights(obj, lon, lat)
+        return obj.weighted(weights).std((lon, lat))
