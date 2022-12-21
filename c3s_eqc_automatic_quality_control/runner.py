6eb4f8ffd0bbc9d738dd6200a81f0f5057d3e53a
Giuseppe Cammarota <g.cammarota@bopen.eu>
2022-12-21 11:10:07 +0100

    Update c3s_eqc_automatic_quality_control/runner.py
    Co-authored-by: Mattia Almansi <m.almansi@bopen.eu>

--- c3s_eqc_automatic_quality_control/runner.py
+++ c3s_eqc_automatic_quality_control/runner.py
@@ -22,7 +22,7 @@ import uuid
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from inspect import getmembers, isfunction
-from typing import Any, Tuple
+from typing import Any
 
 import yaml
 
