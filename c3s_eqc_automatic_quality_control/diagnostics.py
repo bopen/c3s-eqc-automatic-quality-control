687318fc3d514f55ef23cbaa251a9770bb8f9ba6
Mattia Almansi <m.almansi@bopen.eu>
2023-07-14 15:00:43 +0200

    pyupgrade

--- c3s_eqc_automatic_quality_control/diagnostics.py
+++ c3s_eqc_automatic_quality_control/diagnostics.py
@@ -17,8 +17,8 @@ This module gathers available diagnostics.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections.abc import Hashable
-from typing import Any, Callable
+from collections.abc import Callable, Hashable
+from typing import Any
 
 import pyproj
 import xarray as xr
